106223fba151dde696c6d4a922510ebfcc348724
Jurek <jurek.tomasello@gmail.com>
2018-05-11 21:36:04 +0200

    Reset button now working

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 function createGrid(x) {
-    const container = document.querySelector('#container');
+    const container = document.querySelector("#container");
     for(let i = 0; i < x * x; i++) {
       const content = document.createElement("div");
       content.classList.add("gridBox");
       container.appendChild(content);
     }
-    container.setAttribute('style', `grid-template-columns: repeat(${x}, auto)`);
+    container.setAttribute("style", `grid-template-columns: repeat(${x}, auto)`);
   }
 
-  
+function modifyColor() {
+  let grid = document.querySelectorAll(".gridBox");
+  grid.forEach((box) => {
+    box.addEventListener('mouseenter', (e) => {
+      box.classList.add("hoverGridBox");
+    });
+  });
+}
 
-  createGrid(16);
+function clearGrid() {
+  var hoverGrid = document.getElementsByClassName("hoverGridBox");
+  /*document.getElementById("container").innerHTML = "";*/
+  for(i = 0; i < hoverGrid.length; i++) {
+    hoverGrid[i].style.backgroundColor = "purple";
+  
+  /*modifyColor();*/
+  }
+}
+
+document.getElementById("newGridButton").onclick = clearGrid;
+
+createGrid(16);
+modifyColor();
