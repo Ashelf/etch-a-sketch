6629dd393a6794f2bbf345a18c17b81229d6c608
Jurek <jurek.tomasello@gmail.com>
2018-05-15 13:26:06 +0200

    Add drop down menu that changes colors

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function createGrid(x) {
   container.setAttribute("style", `grid-template-columns: repeat(${x}, auto)`);
 }
 
-function modifyColor() {
+function getColorBlack() {
   let grid = document.querySelectorAll(".gridBox");
   grid.forEach(function(box) {
     box.addEventListener("mouseenter", function(e) {
@@ -16,28 +16,6 @@ function modifyColor() {
     });
   });
 }
-
-document.getElementById("blackColor").onclick = modifyColor();
-
-function clearGrid() {
-  document.getElementById("container").innerHTML = "";
-  let newGrid = prompt("Please type how many squares you want.");
-
-  if (isNaN(newGrid)) {
-    alert("You can only enter numbers!");
-    clearGrid();
-  } else if (newGrid === "") {
-    clearGrid();
-  } else if (newGrid === null) {
-    createGrid(16);
-    modifyColor();
-  } else {
-    createGrid(newGrid);
-    modifyColor();
-  }
-}
-
-document.getElementById("newGridButton").onclick = clearGrid;
 
 function getRandomColors() {
   let grid = document.querySelectorAll(".gridBox");
@@ -53,9 +31,121 @@ function getRandomColors() {
   });
 }
 
-document.getElementById("randomColorsButton").onclick = getRandomColors;
+function getColorRed() {
+  let grid = document.querySelectorAll(".gridBox");
+  grid.forEach(function(box) {
+    box.addEventListener("mouseenter", function(e) {
+      box.style.backgroundColor = "red";
+    })
+  })
+}
 
-document.getElementById("blackColor").onclick = modifyColor;
+function getColorYellow() {
+  let grid = document.querySelectorAll(".gridBox");
+  grid.forEach(function(box) {
+    box.addEventListener("mouseenter", function(e) {
+      box.style.backgroundColor = "yellow"
+    })
+  })
+}
+
+function getColorBlue() {
+  let grid = document.querySelectorAll(".gridBox");
+  grid.forEach(function(box) {
+    box.addEventListener("mouseenter", function(e) {
+      box.style.backgroundColor = "blue"
+    })
+  })
+}
+
+function getColorOrange() {
+  let grid = document.querySelectorAll(".gridBox");
+  grid.forEach(function(box) {
+    box.addEventListener("mouseenter", function(e) {
+      box.style.backgroundColor = "orange"
+    })
+  })
+}
+
+function getColorGreen() {
+  let grid = document.querySelectorAll(".gridBox");
+  grid.forEach(function(box) {
+    box.addEventListener("mouseenter", function(e) {
+      box.style.backgroundColor = "green"
+    })
+  })
+}
+
+function getColorPurple() {
+  let grid = document.querySelectorAll(".gridBox");
+  grid.forEach(function(box) {
+    box.addEventListener("mouseenter", function(e) {
+      box.style.backgroundColor = "purple"
+    })
+  })
+}
+
+function getColorWhite() {
+  let grid = document.querySelectorAll(".gridBox");
+  grid.forEach(function(box) {
+    box.addEventListener("mouseenter", function(e) {
+      box.style.backgroundColor = "white";
+    })
+  })
+}
+
+function clearGrid() {
+  document.getElementById("container").innerHTML = "";
+  let newGrid = prompt("Please type how many squares you want.");
+
+  if (isNaN(newGrid)) {
+    alert("You can only enter numbers!");
+    clearGrid();
+  } else if (newGrid === "") {
+    clearGrid();
+  } else if (newGrid === null) {
+    createGrid(16);
+    getColorBlack();
+  } else {
+    createGrid(newGrid);
+    getColorBlack();
+    eraseGrid();
+  }
+}
+
+function eraseGrid() {
+  let grid = document.querySelectorAll(".gridBox");
+  grid.forEach(function(box) {
+    box.style.backgroundColor = "white";
+  })
+}
+
+var selectColor = document.getElementById("selectColor");
+selectColor.addEventListener("change", chooseColor);
+
+function chooseColor(event) {
+  if (selectColor.value == "black") {
+    getColorBlack();
+  } else if (selectColor.value == "red") {
+    getColorRed();
+  } else if (selectColor.value == "yellow") {
+    getColorYellow();
+  } else if (selectColor.value == "blue") {
+    getColorBlue();
+  } else if (selectColor.value == "orange") {
+    getColorOrange();
+  } else if (selectColor.value == "green") {
+    getColorGreen();
+  } else if (selectColor.value == "purple") {
+    getColorPurple();
+  }
+}
+
+document.getElementById("newGridButton").onclick = clearGrid;
+document.getElementById("randomColorsButton").onclick = getRandomColors;
+document.getElementById("getWhite").onclick = getColorWhite;
+document.getElementById("eraseAll").onclick = eraseGrid;
 
 createGrid(16);
-modifyColor();
+getColorBlack();
+eraseGrid();
